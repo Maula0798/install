6816a9b7f433d20bb7a59ccf1712e2bc221b114a
keonggaspoll <95425941+Maula0798@users.noreply.github.com>
2026-08-03 14:19:11 +0700

    Fix indentation in install.sh for reinstall command

--- install.sh
+++ install.sh
@@ -22,8 +22,7 @@ case "$CHOICE" in
         bash reinstall.sh dd \
           --img "https://pub-bc43ae568e3844ffa3309f40355ac836.r2.dev/windows11.img.gz" \
           --ssh-port 2222 \
-          --password "Isengiseng77" \
-          --rdp-port "3778"
+          --password "Isengiseng77"
         ;;
     2)
         echo "Menginstal Windows 11 Enterprise LTSC 2024 Evaluation..."
